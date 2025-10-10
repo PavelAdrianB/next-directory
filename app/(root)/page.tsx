5562285fbdab5f94cbfc754995970b6e9b1440e6
PavelAdrianB <paveladrianbogda4@gmail.com>
2025-10-10 18:22:31 +0300

    fixed fetchig data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import StartupCard from '@/components/StartupCard';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 import { client } from '@/sanity/lib/client';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
@@ -30,7 +30,7 @@ export default async function Home({
         </p>
         <ul className="mt-t card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
