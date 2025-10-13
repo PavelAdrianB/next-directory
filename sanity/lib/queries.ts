b4718df3ce99be344dae0f316263767a3e88363b
PavelAdrianB <paveladrianbogda4@gmail.com>
2025-10-13 16:51:34 +0300

    added startp details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -20,4 +20,28 @@ export const STARTUPS_QUERY =
     category,
     image,
     description
+    
 }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type=="startup" && _id == $id][0]{
+     
+  _id,
+    title,
+    slug,
+    _createdAt,
+    views,
+    author -> {
+      _id, name, image, bio
+    }
+    ,
+    category,
+    image,
+    description,
+    pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type =="startup" && _id == $id][0]{
+  _id, views}
+  `);
